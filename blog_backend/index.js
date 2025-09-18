70bfce45060c7118b401e26c8ab4a4b96c0cfbf4
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-19 01:27:00 +0500

    Setup user,post,commet api endpoint

--- blog_backend/index.js
+++ blog_backend/index.js
@@ -1,6 +1,12 @@
 import express from 'express'
 import cors from 'cors'
 
+import "./mongoose.js"
+
+import userRoutes from './routes/userRoutes.js'
+import postRoutes from './routes/postRoutes.js'
+import commentRoutes from './routes/commentRoutes.js'
+
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -10,6 +16,9 @@ app.get('/',(req,res)=>{
     res.send("Blog Server Running..................")
 })
 
+app.use('/users', userRoutes)       // http://localhost:4000/users
+app.use('/posts', postRoutes)       // http://localhost:4000/posts
+app.use('/comments', commentRoutes)  // http://localhost:4000/comments
 
 const port = 4000
 app.listen(port, '0.0.0.0', () => {
