f12b8f9cb0a4ab408e14163773d0bd47c3d31915
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-20 02:04:11 +0500

    Add Sibgle_post page structure and data fetching

--- blog_frontend/src/pages/SinglePost.jsx
+++ blog_frontend/src/pages/SinglePost.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-const SinglePost = () => {
+export default function SinglePost() {
+  const { id } = useParams();
+  const [post, setPost] = useState(null);
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/posts/${id}`)
+      .then(res => res.json())
+      .then(data => setPost(data));
+
+    fetch(`http://localhost:4000/comments/${id}`)
+      .then(res => res.json())
+      .then(data => setComments(data));
+  }, [id]);
+
+  if (!post) return <p>Loading...</p>;
+
   return (
-    <>
-        <h1>SinglePost</h1>
-    </>
-  )
-}
+    <div className="single-post">
+      <h1>{post.title}</h1>
+      <small>By {post.author} | {new Date(post.date).toLocaleDateString()}</small>
+      <p>{post.content}</p>
 
-export default SinglePost
+      <h3>Comments</h3>
+      {comments.map(c => (
+        <div key={c._id} className="comment">
+          <p>{c.content}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
