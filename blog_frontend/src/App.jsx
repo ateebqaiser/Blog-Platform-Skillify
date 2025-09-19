e6879974c71c75268bb6f1d5ce603cbeee0e07bf
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-20 01:44:29 +0500

    Add routing to pages

--- blog_frontend/src/App.jsx
+++ blog_frontend/src/App.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import SinglePost from "./pages/SinglePost";
 
 const App = () => {
   return (
     <>
-    
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home></Home>} />
+          <Route path="/about" element={<About></About>} />
+          <Route path="/contact" element={<Contact></Contact>} />
+          <Route path="/post:id" element={<SinglePost></SinglePost>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
 
 export default App
+
