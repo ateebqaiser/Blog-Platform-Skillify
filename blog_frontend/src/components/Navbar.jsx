8c08a97abd1ab1e26694581e7b2d9a07826b073f
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-20 01:43:21 +0500

    Add Links to Navbar

--- blog_frontend/src/components/Navbar.jsx
+++ blog_frontend/src/components/Navbar.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+
 
 const Navbar = () => {
   return (
     <>
-        <h1>NavBar</h1>
+        <nav className="navbar">
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
     </>
   )
 }
 
 export default Navbar
+
